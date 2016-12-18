13efdeb17de928c9858a76fc3880eb17af825b11
Edmond Pruteanu <edmond@swiftshift.com>
2016-12-19 00:01:31 +0200

    Added correct endpoint

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 const React = require('react');
+const $ = require('jquery');
 const {Table, Column, Cell} = require('fixed-data-table');
 const loadingCell = <Cell>
     <img width="16" height="16" alt="star" src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
@@ -31,24 +32,23 @@ const cache = {};
 let loading = false;
 const AjaxCell = ({rowIndex, col, forceUpdate, ...props}) => {
     let page = 1;
-    let pageSize = 10;
+    let pageSize = 11;
     let idx = rowIndex;
-    if(rowIndex>=pageSize) {
+    if (rowIndex >= pageSize) {
         page = Math.floor(rowIndex / pageSize) + 1;
         idx = rowIndex % pageSize;
     }
-    if (cache[page]) {
+    if (cache[page] && cache[page][idx]) {
         return <Cell>{cache[page][idx][col]}</Cell>
-    } else if(!loading) {
+    } else if (!loading) {
         console.log("Loading page " + page);
-        loading = true;
+        loading = true;     
 
-        fetch('http://swapi.co/api/people/?format=json&page='+page).then(function(response) {
+        fetch('http://schelepopi.herokuapp.com/clients').then(function(response) {
             return response.json();
         }).then(function(j) {
-            cache[page] = j['results'];
+            cache[page] = j;
             loading = false;
-            forceUpdate();
         });
     }
     return loadingCell;
@@ -63,21 +63,16 @@ class ClientsTable extends React.Component {
   render() {
     return (
       <Table
-          rowHeight={30} rowsCount={87} width={600} height={200} headerHeight={30}>
+          rowHeight={30} rowsCount={11} width={600} height={200} headerHeight={30}>
 
           <Column
-            header={<Cell>Name</Cell>}
+            header={<Cell>Nume</Cell>}
             cell={ <AjaxCell col='name' forceUpdate={this.forceUpdate.bind(this)} /> }
             width={200}
           />
           <Column
-            header={<Cell>Birth Year</Cell>}
-            cell={ <AjaxCell col='birth_year' forceUpdate={this.forceUpdate.bind(this)} /> }
-            width={200}
-          />
-          <Column
-            header={<Cell>Url</Cell>}
-            cell={ <AjaxCell col='url' forceUpdate={this.forceUpdate.bind(this)} /> }
+            header={<Cell>Telefon</Cell>}
+            cell={ <AjaxCell col='mobileNumber' forceUpdate={this.forceUpdate.bind(this)} /> }
             width={200}
           />
       </Table>
